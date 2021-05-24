7ad6f2bb38e8bf3c887149d310d338f2b17c2f6c
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-05-23 23:45:32 -0400

    orgnazing files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
-import NavTabs from "./components/NavTabs";
-import Home from "./components/pages/Home";
-import Projects from "./components/pages/Projects";
-import Footer from "./components/Footer/Footer";
+import NavTabs from "./Components/NavTabs";
+import Home from "./Components/pages/Home";
+import Projects from "./Components/pages/Projects";
+import Footer from "./Components/Footer/Footer";
+
 
 function App() {
   return (
+   
     <Router>
-    <div>
-      <NavTabs />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/projects" component={Projects} />
-    </div>
-    <Footer/>
-  </Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/projects" component={Projects} />
+      </div>
+      <Footer/>
+    </Router>
   );
 }
-
 export default App;
+
