482e36bb74a902cc2353ec8c49cbafc4277cdedc
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-05-23 23:27:10 -0400

    imported my files on the app.js file to render routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import NavTabs from "./components/NavTabs";
+import Home from "./components/pages/Home";
+import Projects from "./components/pages/Projects";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-  
+    <Router>
+    <div>
+      <NavTabs />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/projects" component={Projects} />
+    </div>
+    <Footer/>
+  </Router>
   );
 }
 
