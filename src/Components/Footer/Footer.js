9f5685485f1860a5b00ebe43245f42e28bbd4e1c
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-05-23 23:26:12 -0400

    added my footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../Footer/Footer.css";
+
+
+function Footer() {
+  return (
+   
+    <footer>     
+    <p> Copyright © 2021 Kevin Hidalgo. <img src={"./images/logo.PNG"}></img> </p>
+</footer>  
+    
+  );
+}
+
+export default Footer;
